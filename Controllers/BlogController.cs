875100b7f7307ae0eb7c66f728d334597b69ca7e
nicathuseynli <nijatkhh@code.edu.az>
2023-03-05 10:39:17 +0400

    asp.net

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Data.Entity;
+using Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Dto;
+using Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.UnitOfWork;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Controllers
 {
@@ -7,5 +11,78 @@ namespace Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Controllers
     [ApiController]
     public class BlogController : ControllerBase
     {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<BlogController> _logger;
+
+        public BlogController(IUnitOfWork unitOfWork, ILogger<BlogController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            _logger.LogInformation("Request accepted {0}", DateTime.Now);
+            var blogGetAll = await _unitOfWork.blogRepository.GetAll().Include(x => x.Posts).ToListAsync();
+            _logger.LogWarning("Request Successfuly completed at {0}", DateTime.Now);
+            return Ok(blogGetAll);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] BlogDto blogDto)
+        {
+            Bloog blog = new()
+            {
+                Name = blogDto.Name,
+                Description = blogDto.Description,
+                PostsCount = blogDto.PostCount
+            };
+            await _unitOfWork.blogRepository.Add(blog);
+            await _unitOfWork.Commit();
+            return Ok(blog);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, [FromBody] BlogDto blogDto)
+        {
+            try
+            {
+                _logger.LogInformation($"{id} obtained from base date");
+                var blogUpdate = await _unitOfWork.blogRepository.GetFirst(b => b.Id == id);
+                blogUpdate.Name = blogDto.Name;
+                blogUpdate.Description = blogDto.Description;
+                await _unitOfWork.blogRepository.Update(blogUpdate);
+                await _unitOfWork.Commit();
+                return Ok(blogUpdate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error when looking up id in database id not found {id}");
+                throw;
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                _logger.LogInformation($"{id} obtained from base date");
+
+                var blogUpdate = await _unitOfWork.blogRepository.GetFirst(b => b.Id == id);
+                await _unitOfWork.blogRepository.Delete(blogUpdate);
+                await _unitOfWork.Commit();
+                return Ok(_unitOfWork);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error when looking up id in database id not found {id}");
+                throw;
+            }
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult> GetId(int id)
+        {
+            var blogGetId = await _unitOfWork.blogRepository.Find(id);
+            return Ok(blogGetId);
+        }
     }
 }
--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Data.Entity;
+using Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Dto;
+using Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.UnitOfWork;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Controllers
 {
@@ -7,5 +11,82 @@ namespace Blog.HomeTask.EF.Repository.UnitofWork_04._03._2023.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<PostController> _logger;
+        public PostController(IUnitOfWork unitOfWork, ILogger<PostController> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            _logger.LogInformation("Request accepted {0}", DateTime.Now);
+            var postGetAll = await _unitOfWork.postRepository.GetAll().ToListAsync();
+            _logger.LogWarning("Request Successfuly completed at {0}", DateTime.Now);
+            return Ok(postGetAll);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PostDto postDto)
+        {
+            Post post = new()
+            {
+                Title = postDto.Title,
+                Content = postDto.Content,
+                Desc = postDto.Description,
+                Subtitle = postDto.Subtitle,
+                BlogId = postDto.BlogId
+            };
+
+            await _unitOfWork.postRepository.Add(post);
+            await _unitOfWork.Commit();
+            return Ok(post);
+        }
+        [HttpPut]
+
+        public async Task<IActionResult> Update(int id, PostDto post)
+        {
+            try
+            {
+                _logger.LogInformation($"This post is in the data base {id}");
+                var postUpdate = await _unitOfWork.postRepository.GetFirst(b => b.Id == id);
+                postUpdate.Subtitle = post.Subtitle;
+                postUpdate.Title = post.Title;
+                postUpdate.Content = post.Content;
+                postUpdate.Desc = post.Description;
+                postUpdate.BlogId = post.BlogId;
+                await _unitOfWork.postRepository.Update(postUpdate);
+                await _unitOfWork.Commit();
+                return Ok(postUpdate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error when looking up id in database id not found {id}");
+                throw;
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                _logger.LogInformation($"{id} obtained from base date");
+                var postDelete = await _unitOfWork.postRepository.GetFirst(b => b.Id == id);
+                await _unitOfWork.postRepository.Delete(postDelete);
+                await _unitOfWork.Commit();
+                return Ok(postDelete);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error when looking up id in database id not found {id}");
+                throw;
+            }
+        }
+        [HttpGet("Id")]
+        public async Task<IActionResult> GetId(int id)
+        {
+            var postGetId = await _unitOfWork.postRepository.Find(id);
+            return Ok(postGetId);
+        }
     }
 }
